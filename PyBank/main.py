fe0aa7ebf9b0b9f1082ecf254660ccbfa7c0a12e
alplky <alicia.pelkey@gmail.com>
2020-08-30 22:01:55 +0000

    Adjusted month count using len(list())

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,18 +3,18 @@ import csv
 
 path = os.path.join("Resources", "budget_data.csv")
 
-total_months = (0)
-
 with open(path, "r") as file:
     csv_reader = csv.DictReader(file)
+    
+    total_months = len(list(csv_reader))
     
     for row in csv_reader:
         row = dict(row)
         
-        total_months += 1
-        print(total_months)
+    print(f"Total Months: {total_months}")
         
         
+       
         
     
     
