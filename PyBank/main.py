f1e8a89524c32549ae66c50fd14d0c510f0cb3a0
alplky <alicia.pelkey@gmail.com>
2020-08-31 21:06:59 +0000

    Added comments

--- PyBank/main.py
+++ PyBank/main.py
@@ -5,7 +5,7 @@ out_path = "updated_budget_data.csv"
 out_header = [
     "Date",
     "Profit/Losses",
-    "Profit/Losses_2"
+    "Changes"
 ]
 
 path = os.path.join("Resources", "budget_data.csv")
@@ -25,17 +25,21 @@ with open(path, "r") as in_file, open(out_path, "w+") as out_file:
     for row in dict_reader:
         row = dict(row)
         
-        row["Profit/Losses_2"] = int(row["Profit/Losses"])
-        
-        dict_writer.writerow(row)
-        
         profit_losses = int(row["Profit/Losses"])
         
+        #using append to iterate over lists
         months.append(row["Date"])
         profit_loss.append(profit_losses)
         
+        #Calulation variables to print to terminal
         total_months = len(list(months))
         pl_total = sum(profit_loss)
+        
+        #New row for change in profit/losses over each month
+        row["Changes"] = profit_losses - profit_losses
+        
+        #Create csv with new column of changes over each month
+        dict_writer.writerow(row)
         
     print(f"Financial Analysis")
     print("-" * 25)
