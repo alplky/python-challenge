6fa05b25a140be9c85a95b6cc3a2445eb2b56555
alplky <alicia.pelkey@gmail.com>
2020-08-31 20:23:48 +0000

    Start of creating augmented csv to calculate differences

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,17 +1,50 @@
 import os 
 import csv
 
+out_path = "updated_budget_data.csv"
+out_header = [
+    "Date",
+    "Profit/Losses",
+    "Profit/Losses_2"
+]
+
 path = os.path.join("Resources", "budget_data.csv")
 
-with open(path, "r") as file:
-    csv_reader = csv.DictReader(file)
+months = []
+profit_loss = []
+average_of_changes = []
+greatest_increase = []
+greatest_decrease = []
+
+with open(path, "r") as in_file, open(out_path, "w+") as out_file:
+    dict_reader = csv.DictReader(in_file)
+    dict_writer = csv.DictWriter(out_file, out_header)
     
-    total_months = len(list(csv_reader))
+    dict_writer.writeheader()
     
-    for row in csv_reader:
+    for row in dict_reader:
         row = dict(row)
         
+        row["Profit/Losses_2"] = int(row["Profit/Losses"])
+        
+        dict_writer.writerow(row)
+        
+        profit_losses = int(row["Profit/Losses"])
+        
+        months.append(row["Date"])
+        profit_loss.append(profit_losses)
+        
+        total_months = len(list(months))
+        pl_total = sum(profit_loss)
+        
+    print(f"Financial Analysis")
+    print("-" * 25)
     print(f"Total Months: {total_months}")
+    print(f"Total: ${pl_total}")
+        
+
+      
+    
         
         
        
