84fda04d7f6179eb17823cfb73590128911c028e
alplky <alicia.pelkey@gmail.com>
2020-09-01 22:36:00 +0000

    Added lists a zipped lists to calculate changes across period

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,20 +1,14 @@
 import os 
 import csv
 
-#new csv for calulating monthly changes
-out_path = "updated_budget_data.csv"
-out_header = [
-    "Date",
-    "Profit/Losses",
-    "Changes"
-]
-
 path = os.path.join("Resources", "budget_data.csv")
 
 #empty lists to iterate over for each analysis finding
 months = []
 profit_loss = []
-average_of_changes = []
+prof_loss = []
+prof_loss_2 = []
+augmtd_prof_loss = []
 greatest_increase = []
 greatest_decrease = []
 
@@ -25,6 +19,19 @@ with open(path, "r") as file:
     
     for row in csv_reader:
         
+        #lists for calculating changes between months
+        prof_loss.append(int(row[1]))
+        prof_loss_2.append(int(row[1]))
+        del prof_loss[0]
+        del prof_loss_2[-1]
+        
+        #zipping list together to calculate the difference/change between tuples
+        zipped = zip(prof_loss, prof_loss_2)
+        zip_prof_loss = list(zipped)
+        
+        #calculating the difference/change between tuples
+        changes = [b - a for a, b in zip_prof_loss]
+        
         #using append to iterate over lists
         months.append(row[0])
         profit_loss.append(int(row[1]))
@@ -34,10 +41,12 @@ with open(path, "r") as file:
         pl_total = sum(profit_loss)
     
     #analysis to print to the terminal
+    print("-" * 25)
     print(f"Financial Analysis")
     print("-" * 25)
     print(f"Total Months: {total_months}")
     print(f"Total: ${pl_total}")
+
 
         
 
