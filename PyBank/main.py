d3ad8ca49db64e05a81d27b20e7ada9e9fdff75c
alplky <alicia.pelkey@gmail.com>
2020-09-02 13:20:51 +0000

    removing lists created to find avg change, refactored to use for loop with indexing instead

--- PyBank/main.py
+++ PyBank/main.py
@@ -6,12 +6,9 @@ path = os.path.join("Resources", "budget_data.csv")
 #empty lists to iterate over for each analysis finding
 months = []
 profit_loss = []
-prof_loss = []
-prof_loss_2 = []
-augmtd_prof_loss = []
+changes = []
 greatest_increase = []
 greatest_decrease = []
-
 
 with open(path, "r") as file:
     csv_reader = csv.reader(file)
@@ -19,33 +16,31 @@ with open(path, "r") as file:
     
     for row in csv_reader:
         
-        #lists for calculating changes between months
-        prof_loss.append(int(row[1]))
-        prof_loss_2.append(int(row[1]))
-        del prof_loss[0]
-        del prof_loss_2[-1]
-        
-        #zipping list together to calculate the difference/change between tuples
-        zipped = zip(prof_loss, prof_loss_2)
-        zip_prof_loss = list(zipped)
-        
-        #calculating the difference/change between tuples
-        changes = [b - a for a, b in zip_prof_loss]
-        
         #using append to iterate over lists
         months.append(row[0])
         profit_loss.append(int(row[1]))
         
         #Calulation variables to print to terminal
         total_months = len(list(months))
-        pl_total = sum(profit_loss)
+        pl_total = sum(profit_loss)  
+        
+for i in range(1, len(csv_reader)):
+#     last_period_row = csv_reader[i - 1]
+#     current_period_row = csv_reader[i]
+        
+    changes.append()
     
-    #analysis to print to the terminal
-    print("-" * 25)
-    print(f"Financial Analysis")
-    print("-" * 25)
-    print(f"Total Months: {total_months}")
-    print(f"Total: ${pl_total}")
+#analysis to print to the terminal
+print("-" * 25)
+print(f"Financial Analysis")
+print("-" * 25)
+print(f"Total Months: {total_months}")
+print(f"Total: ${pl_total}")
+print(f"Average Change: ${}")
+print(f"Greatest Increase in Profits: {} ${}")
+print(f"Greatest Increase in Profits: {} ${}")
+
+
 
 
         
