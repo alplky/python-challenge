4fbeef7c55376195e0ec93f214dd39628f3f8b7b
alplky <alicia.pelkey@gmail.com>
2020-09-02 19:33:46 +0000

    Created counter with if/elif statment and added print statements

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -5,6 +5,15 @@ path = os.path.join("Resources", "election_data.csv")
 
 #empty lists to append
 votes = []
+candidates = []
+
+#counter starting at 0 to count each candidates votes
+counter = {
+    "Khan": 0,
+    "Correy": 0,
+    "Li": 0,
+    "OTooley": 0
+}
 
 with open(path, "r") as file:
     csv_reader = csv.reader(file)
@@ -14,8 +23,24 @@ with open(path, "r") as file:
         
         #appending to print calculations
         votes.append(row)
+        candidates.append(row[2])
 
+#adds 1 vote for each candidates name found in candidates 
+for candidate in candidates:
+    if candidate == "Khan":
+        counter["Khan"] += 1
+    elif candidate == "Correy":
+        counter["Correy"] += 1
+    elif candidate == "Li":
+        counter["Li"] += 1
+    elif candidate == "O'Tooley":
+        counter["OTooley"] += 1
+    
 print(f"Election Results")
 print("-" * 25)
-print(F"Total Votes: {len(votes)}")
+print(f"Total Votes: {len(votes)}")
 print("-" * 25)
+print(f"Khan: ({counter['Khan']})")
+print(f"Correy: ({counter['Correy']})")
+print(f"Li: ({counter['Li']})")
+print(f"O'Tooley: ({counter['OTooley']})")
