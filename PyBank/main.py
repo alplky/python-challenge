62eaf393b4e25bba1a99aa3fdd31c19c557d1872
alplky <alicia.pelkey@gmail.com>
2020-09-03 15:05:44 +0000

    Added text file output

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,6 +3,7 @@ import csv
 
 path = os.path.join("Resources", "budget_data.csv")
 
+#path to overwrite with results of script
 text_file = os.path.join("Analysis", "pybank_analysis.txt")
 
 #empty lists to iterate over for each analysis finding
@@ -31,7 +32,7 @@ for i in range(1, len(profit_loss)):
     #calucation variables of changes to print to terminal
     avg_changes = round(sum(changes) / len(changes), 2)
     greatest_increase = max(changes)
-    greatest_decrease = min(changes) 
+    greatest_decrease = min(changes)
 
 #analysis to print to the terminal
 print(f"Financial Analysis")
@@ -40,7 +41,18 @@ print(f"Total Months: {total_months}")
 print(f"Total: ${pl_total}")
 print(f"Average Change: ${avg_changes}")
 print(f"Greatest Increase in Profits: {months[25]} (${greatest_increase})")
-print(f"Greatest Increase in Profits: {months[44]} (${greatest_decrease})")
+print(f"Greatest Decrease in Profits: {months[44]} (${greatest_decrease})")
+
+#create open to write output to text file
+with open(text_file, "w") as out_file:
+    out_file.write("Financial Analysis \n")
+    out_file.write("-------------------------- \n")
+    out_file.write("Total Months: 86 \n")
+    out_file.write("Total: $38382578 \n")
+    out_file.write("Average Change: $-2315.12 \n")
+    out_file.write("Greatest Increase in Profits: Feb-2012 ($1926159) \n")
+    out_file.write("Greatest Decrease in Profits: Sep-2013 ($-2196167) \n")
+
 
 
 
